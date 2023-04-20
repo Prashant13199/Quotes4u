1641e596869c8b5af55b0abe1ee139a29b5800bf
Prashant Kumar <prashantkumar@Prashants-MacBook-Pro.local>
2023-04-20 12:45:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import data from './quotes.json'
 
 function App() {
+
+  const [number, setNumber] = useState("")
+
+  useEffect(() => {
+    setNumber(Math.floor(Math.random() * data.quotes.length))
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {number && data.quotes[number].quote}
+        <div className='author'>
+          - {number && data.quotes[number].author ? data.quotes[number].author : 'Unknown'}
+        </div>
+      </div>
     </div>
   );
 }
